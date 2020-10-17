fb335c4ec5d345a40ed5456eb796a79707e030f1
Ayatea <ayatea1223@gmail.com>
2020-10-17 17:13:59 +0900

    :sparkles: [#2] CSVファイルの読み込みDemoを実装

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,2 +1,9 @@
+require 'csv'
+
 class FilesController < ApplicationController
+  # 指定パスのCSVファイルを読み込む
+  # @return [Array] 読み込んだCSVデータ
+  def read_csv
+    @results = CSV.read("#{Rails.root}/tmp/files/test_file.csv")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'top#index'
-  resources :files, only: [:index]
+  resources :files, only: [:index] do
+    collection do
+      get :read_csv
+    end
+  end
 end
